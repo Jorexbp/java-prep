dfb914f1d7001a4fd4c994aa53189befd2d6a1bd
Jorexbp <jorgebp2004@gmail.com>
2025-10-01 01:08:54 +0200

    Initial commit

---
+++ src/base/colecciones/Intro.java
@@ -0,0 +1,36 @@
+package base.colecciones;
+
+public class Intro {
+    /*
+        En Java existe el JCF (Java Collections Framework), un conjunto de clases e interfaces
+        que proveen estructuras de datos listas para usar de forma eficiente.
+
+        Dentro del JCF encontramos principalmente las siguientes interfaces:
+        - List
+        - Set
+        - Queue
+        - Map (no hereda de Collection, pero forma parte del framework)
+
+        Algunas implementaciones comunes son: ArrayList, HashSet y HashMap.
+
+        A diferencia de los arrays, las colecciones permiten un redimensionamiento dinámico.
+
+        El JCF también incluye algoritmos útiles como ordenamiento, búsqueda e iteración.
+    */
+
+
+
+    /*                              CORE INTERFACES                              */
+
+    /*
+        La base del JFC esta construido sobre interfaces como Collection, List, Set, Queue, Deque y Map
+        En cada clase de este paquete se define cada interfaz en detalle con ejemplos :)
+
+        - Collection interface
+        - List interface
+        - Set interface
+        - Queue interface
+        - Deque interface
+        - Map interface
+     */
+}
